e2219a268efb5b38fca0b707730dd567fcf54b49
Vishal Jadoun <vishaljadoun2007@gmail.com>
2025-06-15 19:58:38 +0000

    basic of js starting

---
+++ js_learning/01_basics.js
@@ -0,0 +1,20 @@
+const accountId = 14456;
+let accountEmail = "vishal@gmail.com";
+var accountPassowrd = "0690";
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 54
+
+accountEmail = "v@v.com";
+accountPassowrd = "143";
+accountCity = "Bihar";
+
+console.log(accountId);
+
+/*
+preffer not to use var
+because issue in the block scope and functional scope
+*/
+
+console.table([accountEmail, accountId, accountPassowrd, accountCity, accountState]);
