19e7c3480dab0a4eb2f6a487b66298bba8b66ca9
Vishal Jadoun <vishaljadoun2007@gmail.com>
2025-06-15 19:58:54 +0000

    datatypes

---
+++ js_learning/02_database.js
@@ -0,0 +1,30 @@
+"use strict"; // trest all JS code as newer version
+
+// alert(3+3) // we are using node.js , not browser
+
+console.log(3
+    +
+    3 ) // code readbility that should be high ,,,, the code still works but not readable friedly
+
+console.log("Vishal");
+
+let name = "vishal"
+let age = 69
+let isLoggedIn = false
+
+// number => 2 to power 53 
+// bigint 
+// string  => preffer to use in " "
+// boolean => true/fale
+// null => standalone value
+// undefiend => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof undefined); // output => undefined
+console.log(typeof null); // output => object
+
+
+console.table([age, name, isLoggedIn]);
