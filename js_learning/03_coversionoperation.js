a604109629df46809afd88f79e9ba612ea43fbb0
Vishal Jadoun <vishaljadoun2007@gmail.com>
2025-06-16 14:27:46 +0000

    coverstion of data type

---
+++ js_learning/03_coversionoperation.js
@@ -0,0 +1,30 @@
+let score = "vishal"  // its a variable variable quantity and be a number alphabat or NaN
+
+console.log(typeof score); //to check the type of the score value weather is a string or bollean etc
+console.log(typeof (score)); // just a way to to rewrite the upper code
+
+let valueInNumber = Number(score) // now we are putting the score wale with a let consant to get it for its only a number or NaN by using valueInNumber prompt
+console.log(typeof valueInNumber); // checking the type of the prompt
+console.log(valueInNumber); // NaN (not a number) ....... checking the the real value weather its a number or NaN
+
+// "33" => 33 output wil be a nunmber\
+// "33abc" => NaN (bcz there are alphabatical letter with number) 
+// true => 1; fale => 0
+
+
+let isLoggedIn = "v"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// if we but '1' in boolean we get 'true'
+// if we but '0' in boolean we get 'false'
+// "" (enpty string) we get false
+// "_" (string with any kind of value) we get true
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
