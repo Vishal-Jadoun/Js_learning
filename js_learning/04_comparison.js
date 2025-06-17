314e31a30d332cd9b1b20af67c3fffeb5845f9b3
Vishal Jadoun <vishaljadoun2007@gmail.com>
2025-06-17 18:02:38 +0000

    comparision

---
+++ js_learning/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);  // try not to do these type conversion it may cause confusion and let to mess ups (8-18)
+//try to right clean code it's more aprictiable
+
+// in js comparision and equality check are two different value
+
+// === dont just check value instead check vlaue with strictness
+
+console.log("2" === 2);
